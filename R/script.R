5d5a444bd25dfa9083b78c1752ba0212fae62022
rayanne-matos <rayanne.andradebio@gmail.com>
2020-02-11 18:30:01 -0300

    dados e script

---
+++ R/script.R
@@ -0,0 +1,9 @@
+# script para ler a tabela limpa
+# os dados originais estao num slsx
+
+read.table
+help("read.table")
+
+data_format <- read.table("data/dadosiris.txt")
+read.table("data/dadosiris.txt")
+
